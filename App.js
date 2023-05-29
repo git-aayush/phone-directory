6770b03d33629a425d155419b65eea1fd23ee95e
Aayush tyagi <112718435+git-aayush@users.noreply.github.com>
2023-05-29 20:52:39 +0530

    Created Skeleton of Home Page

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-      <div>
-
-
+  return (
+    // <div>
+    <fragment>
+      <div className='Header'>
+        Phone Directory
       </div>
-    );
-  }
+      <button> Add </button>
+      <div>
+        <span> Name </span> <br />
+        <span> Phone </span>
+      </div>
+      <label htmlFor="name"> Name: </label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue="Aayush"/>
+      </fragment>
+    // </div>
+  )
+}
+
 export default App;
