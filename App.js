7f925ea38962ef63698cc7100bb5d5d1ef12e1df
Aayush tyagi <112718435+git-aayush@users.noreply.github.com>
2023-05-30 00:41:18 +0530

    Added ‘Delete’ button

--- App.js
+++ App.js
@@ -3,17 +3,43 @@ import './App.css';
 import Header from './Header.js';
 
 function App() {
+  let subscribers = [
+    {
+      id: 1,
+      name: "Aayush",
+      phone: "8080808080",
+    },
+    {
+      id: 2,
+      name: "Vaibhav",
+      phone: "9090909090",
+    }
+  ]
   return (
-     <div>
-      <div className='Header'>
-        Phone Directory
+    <div>
+      <Header />
+      <div className='component-body-container'>
+        <button className='custom-btn add-btn'>Add</button>
+
+        <div className='grid-container heading-container'>
+          <span className='grid-item name-heading'>Name</span>
+          <spam className='grid-continer phone-heading'>Phone</spam>
+        </div>
+
+        {
+          subscribers.map(sub => {
+            return <div key={sub.id} className="grid-container">
+              <span className='grid-item'>{sub.name}</span>
+              <span className='grid-item'>{sub.phone}</span>
+              <span className='grid-item action-btn-container'>
+                <button className='custom-btn delete-btn'>Delete</button>
+              </span>
+            </div>
+          })
+        }
+
       </div>
-      <button> Add </button>
-      <div>
-        <span> Name </span> <br />
-        <span> Phone </span>
-      </div>
-       </div>
+    </div>
 
 
   );
